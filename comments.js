8b01a0287417313a665ba7bde4f244e16fc6e0f5
Abdenour Bouziane <109435300+AbdenourBouziane@users.noreply.github.com>
2024-03-28 13:38:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,7 @@
 // create a web server 
-const http = require('http');
+import { createServer } from 'http';
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
